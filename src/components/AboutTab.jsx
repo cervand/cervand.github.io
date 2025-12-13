515f695e0f249cffbc86a86eedc91d77b52083b5
Andres <andres.ac.cervantes@gmail.com>
2025-12-12 20:23:39 -0800

    Refactor AboutTab component to use Grid layout and add profile image

--- src/components/AboutTab.jsx
+++ src/components/AboutTab.jsx
@@ -1,14 +1,26 @@
 // src/components/TabContent/AboutTab.jsx
-import { Title, Text, Stack } from '@mantine/core';
+import { Title, Text, Stack, Grid, Image } from '@mantine/core';
 
 function AboutTab() {
   return (
-    <Stack>
-      <Title order={2} mb="md" c="brand.2">About</Title>
-      <Text c="brand.2">
-        Someone excited to work hard on becoming a better engineer.
-      </Text>
-    </Stack>
+    <Grid gutter={50} align="center">
+      <Grid.Col span={{ base: 12, md: 6 }}>
+        <Image 
+          src="https://placehold.co/700x700" 
+          alt="Profile" 
+          radius="md"
+        />
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, md: 6 }}>
+        <Stack>
+          <Title order={2} c="brand.2">Hi, I am Andres</Title>
+          <Text c="brand.2">
+            Someone excited to work hard on becoming a better engineer.
+            This is a summary about me and my background.
+          </Text>
+        </Stack>
+      </Grid.Col>
+    </Grid>
   );
 }
 

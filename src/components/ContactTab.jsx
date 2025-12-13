8c0ecc2252938bbead4a7bbb47f70c4b1ac36d29
Andres <andres.ac.cervantes@gmail.com>
2025-12-12 21:18:29 -0800

    Add @tabler/icons-react dependency and update ContactTab component layout

--- src/components/ContactTab.jsx
+++ src/components/ContactTab.jsx
@@ -1,12 +1,21 @@
 // src/components/TabContent/ContactTab.jsx
-import { Title, Anchor, Stack } from '@mantine/core';
+import { Title, Anchor, Stack, Group, rem } from '@mantine/core';
+import { IconBrandLinkedin, IconBrandGithub } from '@tabler/icons-react';
 
 function ContactTab() {
   return (
     <Stack>
       <Title order={2} mb="md" c="brand.2">Contact</Title>
-      <Anchor href="https://linkedin.com/in/cervand" target="_blank" c="brand.1">LinkedIn Profile</Anchor>
-      <Anchor href="https://github.com/cervand" target="_blank" c="brand.1">GitHub Profile</Anchor>
+      
+      <Group>
+        <IconBrandLinkedin size={24} color="#0A66C2" />
+        <Anchor href="https://linkedin.com/in/cervand" target="_blank" c="brand.1">LinkedIn Profile</Anchor>
+      </Group>
+
+      <Group>
+        <IconBrandGithub size={24} color="black" />
+        <Anchor href="https://github.com/cervand" target="_blank" c="brand.1">GitHub Profile</Anchor>
+      </Group>
     </Stack>
   );
 }

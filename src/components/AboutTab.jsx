5021455938cdc144e354b9f9611f835fbcc7ab9e
Andres <andres.ac.cervantes@gmail.com>
2025-12-12 20:36:53 -0800

    Add profile and graduation images to AboutTab component

--- src/components/AboutTab.jsx
+++ src/components/AboutTab.jsx
@@ -1,14 +1,25 @@
 // src/components/TabContent/AboutTab.jsx
 import { Title, Text, Stack, Grid, Image } from '@mantine/core';
+import profilePic from '../assets/profile.jpg';
+import gradPic from '../assets/gradPic.jpg';
 
 function AboutTab() {
+  /*
+  Notes for me...
+  base = mobile
+  md = tablet and up
+  12 = 100% width
+  6 = 50% width
+  gutter = space between columns
+  */
   return (
+    <>
     <Grid gutter={50} align="center">
       <Grid.Col span={{ base: 12, md: 6 }}>
         <Image 
-          src="https://placehold.co/700x700" 
-          alt="Profile" 
-          radius="md"
+          src = {profilePic}
+          alt="Profile headshot of Andres" 
+          radius="xl"
         />
       </Grid.Col>
       <Grid.Col span={{ base: 12, md: 6 }}>
@@ -21,6 +32,30 @@ function AboutTab() {
         </Stack>
       </Grid.Col>
     </Grid>
+
+    {/* A value of mt 30 means margin-top of 30 pixels */}
+    <Grid gutter={50} align="center" mt={30}>
+      <Grid.Col span={{ base: 12, md: 6 }}>
+        <Stack>
+          <Title order={3} c="brand.2">Background</Title>
+          <Text c="brand.2">
+            I graduated from the University of California, Irvine in 2022 with a Bachelor of Science in Computer Engineering.
+          </Text>
+        </Stack>
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, md: 6 }}>
+        <Image 
+          src={gradPic}
+          alt="Graduation picture of Andres in front of the UCI Engineering School sign" 
+          radius="xl"
+        />
+      </Grid.Col>
+    </Grid> 
+
+
+
+  </>
+
   );
 }
 

9c4eec96914d1f35f9302a25da67578b5dbc38d9
Andres <andres.ac.cervantes@gmail.com>
2025-12-13 02:41:23 -0800

    Update link text in ContactTab component for clarity

--- src/components/ContactTab.jsx
+++ src/components/ContactTab.jsx
@@ -9,12 +9,12 @@ function ContactTab() {
       
       <Group>
         <IconBrandLinkedin size={24} color="#0A66C2" />
-        <Anchor href="https://linkedin.com/in/cervand" target="_blank" c="brand.1">LinkedIn Profile</Anchor>
+        <Anchor href="https://linkedin.com/in/cervand" target="_blank" c="brand.1">LinkedIn</Anchor>
       </Group>
 
       <Group>
         <IconBrandGithub size={24} color="black" />
-        <Anchor href="https://github.com/cervand" target="_blank" c="brand.1">GitHub Profile</Anchor>
+        <Anchor href="https://github.com/cervand" target="_blank" c="brand.1">GitHub</Anchor>
       </Group>
     </Stack>
   );
